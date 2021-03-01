bd765ae61a22f8c0aa710c02303ff90b2a3ae734
cjh8817676 <jonathan1446171@gmail.com>
2021-03-01 10:45:08 +0800

    just push

---
+++ 8_function_exploration/2_reference_to_array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+
+    char pt[40];
+    char & rodents = pt;  
+    char * xd;
+    xd =pt;
+
+    // reference 不可以參考 array。
+    // 指標 可以 指向 array。  
+}
